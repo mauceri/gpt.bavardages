f8ad510480e5b2f59acff56560c5497ecfdbf0e4
Christian Mauceri <christian@mauceri.me>
2023-01-21 23:48:55 +0100

    Marre de Facebook

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -29,10 +29,10 @@ export const authOptions: NextAuthOptions = {
       },
     }),
     */
-    FacebookProvider({
+    /*FacebookProvider({
       clientId: process.env.FACEBOOK_ID as string,
       clientSecret: process.env.FACEBOOK_SECRET as string,
-    }),
+    }),*/
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
