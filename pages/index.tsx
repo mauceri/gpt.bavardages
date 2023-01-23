7a2b64c980a2041d8faeac901fea1029aad96a43
Christian Mauceri <christian@mauceri.me>
2023-01-23 19:12:18 +0100

    Habillage Terminal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,64 @@
 //import Head from 'next/head'
 import Layout from 'components/layout'
-import Terminal from 'components/terminal'
-import SignIn from 'components/signin'
 import { useSession, getSession } from "next-auth/react"
 
+import React, {useEffect, useMemo} from 'react';
+import Terminal from 'components/terminal';
+import {useTerminal} from "components/hooks";
 
 export default function Home() {
+  const {
+    history,
+    pushToHistory,
+    setTerminalRef,
+    resetTerminal,
+  } = useTerminal();
+
+
+  useEffect(() => {
+    resetTerminal();
+
+    pushToHistory(<>
+        <div><strong>Welcome!</strong> to the terminal.</div>
+        <div style={{fontSize: 20}}>It contains <span style={{color: 'yellow'}}><strong>HTML</strong></span>. Awesome, right?</div>
+        <br/>
+        <div>You can write: start or alert , to execute some commands.</div>
+      </>
+    );
+  }, []);
+
+  const commands = useMemo(() => ({
+    'start': async () => {
+      await pushToHistory(<>
+          <div>
+            <strong>Starting</strong> the server... <span style={{color: 'green'}}>Done</span>
+          </div>
+        </>);
+    },
+    'alert': async () => {
+      alert('Hello!');
+      await pushToHistory(<>
+          <div>
+            <strong>Alert</strong>
+            <span style={{color: 'orange', marginLeft: 10}}>
+              <strong>Shown in the browser</strong>
+            </span>
+          </div>
+        </>);
+    },
+  }), [pushToHistory]);
+
+
   const { data: session, status } = useSession()
 
   return (
     <Layout>
-      <Terminal />
+      <Terminal
+        history={history}
+        ref={setTerminalRef}
+        promptLabel={<>Write something awesome:</>}
+        commands={commands}
+      />
     </Layout>
   )
 }
