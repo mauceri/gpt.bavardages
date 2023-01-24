d082e045c9ff8709d68ef19a6e1f0ed9428e68ee
Christian Mauceri <christian@mauceri.me>
2023-01-24 17:25:06 +0100

    Connexion OpenAI Vercel

--- components/hooks.tsx
+++ components/hooks.tsx
@@ -50,7 +50,6 @@ export const useTerminal = () => {
 
   return {
     history,
-    pushToHistory,
     terminalRef,
     setTerminalRef,
     resetTerminal,
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,23 +13,9 @@ import { useTerminal } from 'components/hooks';
 export default function Home() {
   const {
     history,
-    pushToHistory,
     setTerminalRef,
     resetTerminal,
   } = useTerminal();
-
-
-  useEffect(() => {
-    resetTerminal();
-
-    pushToHistory(<>
-      <div><strong>Bienvenue sur bavardages.org</strong></div>
-      <div>Ici, il sera bientôt possible de <span style={{ color: 'yellow', fontSize: 20 }}><strong>bavarder</strong></span> avec un robot d'OpenAI</div>
-      <br />
-      <div>Pour l'instant je suis en mode perroquet...</div>
-    </>
-    );
-  }, []);
 
 
   
