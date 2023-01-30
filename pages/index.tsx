fec3d9673cde15396cad9779f7434fe8fcbe8582
Mauceri <cmauceri@gmail.com>
2023-01-30 17:49:59 +0100

    Installation clerk de base

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,23 @@
 //import Head from 'next/head'
 import Layout from 'components/layout'
 import Terminal from 'components/terminal';
+import {
+  ClerkProvider,
+  SignedIn,
+  SignedOut,
+  UserButton,
+  useUser,
+  RedirectToSignIn,
+} from "@clerk/clerk-react";
 
 export default function Home() {
+  const clerk_pub_key = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY as string;
+  console.log("la clef publique %s", clerk_pub_key)
   return (
-    <Layout>
-      <Terminal />
-    </Layout>
-    )
+    <ClerkProvider publishableKey={clerk_pub_key}>
+       <Layout>
+        <Terminal />
+      </Layout>
+    </ClerkProvider>
+  )
 }
