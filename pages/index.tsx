4fc3a9c24e2b672ff6fddb884f5f276c4e2edeec
Mauceri <cmauceri@gmail.com>
2023-01-31 09:38:05 +0100

    Variables d'environnement Vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,10 +11,10 @@ import {
 } from "@clerk/clerk-react";
 
 export default function Home() {
-  //const clerk_pub_key = process.env.REACT_APP_CLERK_PUBLISHABLE_KEY;
-  //console.log("la clef publique %s", clerk_pub_key)
+  const clerk_pub_key = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY;
+  console.log("la clef publique %s", clerk_pub_key)
   return (
-    <ClerkProvider publishableKey={"pk_test_ZW5vdWdoLXJpbmd0YWlsLTY3LmNsZXJrLmFjY291bnRzLmRldiQ"}>
+    <ClerkProvider publishableKey={clerk_pub_key  as string}>
        <Layout>
         <Terminal />
       </Layout>
