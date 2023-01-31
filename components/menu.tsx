4535194d7b1067fc2575492b4dcfa77e56e48f97
Mauceri <cmauceri@gmail.com>
2023-01-31 09:33:52 +0100

    Variables d'environnement Vercel

--- components/menu.tsx
+++ components/menu.tsx
@@ -13,18 +13,19 @@ import {
 
 export default function Menu() {
     const { user } = useUser();
+    console.log("id = %s", user?.id);
     return (
         < div className="menu" >
-        <SignedIn>
-            {user ?
-                <><UserButton /><p>{user.firstName}</p></> : null
-            }
-        </SignedIn>
-        <SignedOut>
-            <RedirectToSignIn />
-         </SignedOut>
- 
+            <SignedIn>
+                {user ?
+                    <><UserButton /><p>{user.firstName}</p></> : null
+                }
+            </SignedIn>
+            <SignedOut>
+                <RedirectToSignIn />
+            </SignedOut>
+
         </div>
-        
+
     )
 }
