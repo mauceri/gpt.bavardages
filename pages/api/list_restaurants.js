1f2a47f3a0fa6e5b404ebac5765ddaeb83b01c05
Mauceri <cmauceri@gmail.com>
2023-02-13 08:46:06 +0100

    Début du traitement des contextes

--- pages/api/list_restaurants.js
+++ pages/api/list_restaurants.js
@@ -12,6 +12,7 @@ export default async function handler(req, res) {
             .toArray();
 
         res.json(results);
+        
         console.log("done")
     } catch (e) {
         console.error(e);
