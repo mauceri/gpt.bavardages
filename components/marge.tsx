7ae0e7fc942807e85f1a897a4977909feeb7440e
Mauceri <cmauceri@gmail.com>
2023-02-17 23:16:43 +0100

    Ant design premiers pas

--- components/marge.tsx
+++ components/marge.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import Menu from './menu'
+import Menu from './menu-clerk'
 import Contextes from './contextes'
 
 function youClickedMe() {
