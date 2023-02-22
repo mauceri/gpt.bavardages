a7a825e86f26f34a55e26d382556bae0041c080a
Mauceri <cmauceri@gmail.com>
2023-02-22 11:10:35 +0100

    Nettoyage AntdList

--- components/testAntdList.tsx
+++ components/testAntdList.tsx
@@ -10,22 +10,10 @@ import {
 import { useUser } from '@clerk/clerk-react';
 
 interface DataType {
-  gender?: string;
-  name: {
-    title?: string;
-    first?: string;
-    last?: string;
-  };
-  email?: string;
-  picture: {
-    large?: string;
-    medium?: string;
-    thumbnail?: string;
-  };
-  nat?: string;
+  name: string;
+  date?: string;
   loading: boolean;
 }
-
 const count = 3;
 const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
 interface AntdListProps {
@@ -38,61 +26,22 @@ const AntdList: React.FC<AntdListProps> = ((props) => {
   const [data, setData] = useState<DataType[]>([]);
   const [list, setList] = useState<DataType[]>([]);
   const { isLoaded, isSignedIn, user } = useUser();
-  const [contextes, setContextes] = useState([]);
+  const [contextes, setContextes] = useState<DataType[]>([]);
 
   useEffect(() => {
-    (async () => {
-
-      if (user) {
-        const results = await fetch("/api/queryMDB?op=list_contexts&user=" + user?.id).then(response => response.json());
-        setContextes(results);
-      }
-    })();
+    if (user) {
+      fetch("/api/queryMDB?op=list_contexts&user=" + user?.id)
+        .then((res) => res.json())
+        .then((res) => {
+          setInitLoading(false);
+          setData(res);
+          setContextes(res);
+        });
+    }
   }, [user]);
 
-  useEffect(() => {
-    fetch(fakeDataUrl)
-      .then((res) => res.json())
-      .then((res) => {
-        setInitLoading(false);
-        setData(res.results);
-        setList(res.results);
-      });
-  }, []);
-
-  const onLoadMore = () => {
-    setLoading(true);
-    setList(
-      data.concat([...new Array(count)].map(() => ({ loading: true, name: {}, picture: {} }))),
-    );
-    fetch(fakeDataUrl)
-      .then((res) => res.json())
-      .then((res) => {
-        const newData = data.concat(res.results);
-        setData(newData);
-        setList(newData);
-        setLoading(false);
-        // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
-        // In real scene, you can using public method of react-virtualized:
-        // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
-        window.dispatchEvent(new Event('resize'));
-      });
-  };
-
-  const loadMore =
-    !initLoading && !loading ? (
-      <div
-        style={{
-          textAlign: 'center',
-          marginTop: 12,
-          height: 32,
-          lineHeight: '32px',
-        }}
-      >
-        <Button onClick={onLoadMore}>Encore</Button>
-      </div>
-    ) : null;
-
+  
+  
   return (
     <div
       id="scrollableDiv"
@@ -101,10 +50,10 @@ const AntdList: React.FC<AntdListProps> = ((props) => {
         overflow: 'scroll',
       }}>
       <div style={{ fontSize: "16px", color: "blue", width: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-       
+
         <Button key="new" icon={<PlusSquareOutlined style={{ fontSize: '10px' }} />} />
         <span>  Nouveau bavardage </span>
-        
+
       </div>
       <List
         className="demo-loadmore-list"
@@ -115,7 +64,7 @@ const AntdList: React.FC<AntdListProps> = ((props) => {
         itemLayout="vertical"
         //loadMore={loadMore}
         dataSource={contextes}
-        renderItem={(item) => (
+        renderItem={(item: DataType) => (
           <List.Item
             actions={[
               <Space key={1}>
@@ -123,14 +72,12 @@ const AntdList: React.FC<AntdListProps> = ((props) => {
                 <Button key="delete" danger icon={<DeleteFilled style={{ fontSize: '10px' }} />} />
               </Space>,
             ]}>
-            <Skeleton avatar title={false} 
-            //</List.Item>loading={item.loading} 
-            active>
+            <Skeleton avatar title={false} loading={item.loading} active>
               <List.Item.Meta
                 avatar={<Button key="edit" icon={<MessageOutlined style={{ fontSize: '10px' }} />} />}
                 title={
                   <div style={{ width: '100%', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                    {item.name}
+                    {item.name} {item.date}
                   </div>
                 }
               />
