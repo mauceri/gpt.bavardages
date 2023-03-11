f194ca63741d7c624c4223b32cccbaf12cb9bd1e
Mauceri <cmauceri@gmail.com>
2023-03-11 11:10:18 +0100

    upload.ts

---
+++ pages/api/upload.ts
@@ -0,0 +1,19 @@
+import formidable from "formidable";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req:NextApiRequest, res:NextApiResponse) => {
+  const form = formidable({ multiples: true });
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Une erreur s'est produite lors de l'upload du fichier.");
+      return;
+    }
+
+    // Ici, vous pouvez faire ce que vous voulez avec les fichiers uploadés
+    console.log(files);
+
+    res.status(200).send("Fichier uploadé avec succès !");
+  });
+};
